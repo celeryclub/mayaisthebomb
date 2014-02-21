1a47489b34186e0c6804b92003e5300a0229f1fe
Stephen Davis <stephendavis89@gmail.com>
2014-02-21 14:47:21 -0500

    Add admin section for reasons

--- app/models/Reason.php
+++ app/models/Reason.php
@@ -2,4 +2,8 @@
 
 class Reason extends Model {
     public static $_table = 'reasons';
+
+    public function update($attributes) {
+        $this->hydrate(array_merge($this->as_array(), $attributes));
+    }
 }
