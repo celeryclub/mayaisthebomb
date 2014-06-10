e05b1ea160fecc797b73567bba88883de4cb50ff
Stephen Davis <stephendavis89@gmail.com>
2014-06-10 09:59:23 -0400

    Pull Reasons from database

--- app/routes/public.php
+++ app/routes/public.php
@@ -1,5 +1,7 @@
 <?php
 
 $app->get('/', function() use($app) {
-  return $app->render('index.twig');
+  return $app->render('index.twig', array(
+    'reasons' => Reason::find()->order('number desc')
+  ));
 });
